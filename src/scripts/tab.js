d328cb3ca654b56691c1cd24f769bd7b4c00b028
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2019-06-02 01:07:19 +0300

    Changes in html js and styles

---
+++ src/scripts/tab.js
@@ -0,0 +1,37 @@
+console.log('test');
+
+var tabContent = function () {
+  let lists = document.querySelectorAll('.accordion a');
+  let rowItems = document.getElementsByClassName('items_active');
+  let numberOfRowItems = rowItems.length;
+
+  let listClickBack = function (item) {
+    this.item = item;
+    var self = this.item;
+    self.classList.add('active');
+
+    for (var i = 0; i < lists.length; i++) {
+      if (lists[i].classList.contains('active') && lists[i] !== self) {
+        lists[i].classList.remove('active');
+      }
+    }
+  };
+
+  let showPictures = function (element) {
+    let currentHref = element.getAttribute('href');
+    // remove all show
+    for (var i = 0; i < numberOfRowItems; i++) {
+      document.querySelector('#' + rowItems[i].id).classList.remove('show');
+    }
+    document.querySelector(currentHref).classList.add('show');
+  };
+
+  for (var i = 0; i < lists.length; i++) {
+    lists[i].addEventListener('click', function () {
+      listClickBack(this);
+      showPictures(this);
+    });
+  }
+};
+
+tabContent();
