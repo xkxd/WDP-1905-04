c1ff9e1241cc6ceef8059cd6f0483e8087e06c99
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2019-06-14 21:54:33 +0300

    Correct stars.js

--- src/scripts/stars.js
+++ src/scripts/stars.js
@@ -1,18 +1,18 @@
 'use strict';
 
 let starsAll = function () {
-  let stars = document.querySelectorAll('.stars');
+  const stars = document.querySelectorAll('.stars');
 
   function counterStars (event) {
     let childStar = event.target;
-    let parentStar = childStar.parentNode;
-    let childrenStar = parentStar.children;
+    const parentStar = childStar.parentNode;
+    const childrenStar = parentStar.children;
     let counterStar = 0;
 
     if (childStar.classList.contains('star')) {
       // Counting nr of stars
       while ((childStar = childStar.previousElementSibling) != null) {
-        counterStar++;
+        counterStar += 1;
         for (let x = 0; x <= counterStar; x++) {
           childrenStar[x].classList.add('rated-star');
           childrenStar[x].style.color = '#d58e32';
@@ -29,37 +29,37 @@ let starsAll = function () {
     let children = event.target.parentNode.children;
     if (event.target.classList.contains('star')) {
       for (let y = 0; y < children.length; y++) {
-        children[y].classList.remove('rated-star', 'full', 'hoverOn');
+        children[y].classList.remove('rated-star', 'full', 'hover-on');
       }
     }
   }
 
   function hoverOn (event) {
     let hStar = event.target;
-    let hParent = hStar.parentNode;
-    let hChildren = hParent.children;
+    const hParent = hStar.parentNode;
+    const hChildren = hParent.children;
     let hCounter = 0;
 
     if (hStar.classList.contains('star')) {
       while ((hStar = hStar.previousElementSibling) != null) {
-        hCounter++;
+        hCounter += 1;
         for (let z = 0; z <= hCounter; z++) {
-          hChildren[z].classList.add('hoverOn');
+          hChildren[z].classList.add('hover-on');
           hChildren[z].style.color = '#d58e32';
         }
       }
       if (event.target === hParent.firstElementChild) {
-        event.target.classList.add('hoverOn');
+        event.target.classList.add('hover-on');
         event.target.style.color = '#d58e32';
       }
     }
   }
 
   function hoverOut (event) {
-    let children = event.target.parentNode.children;
+    const children = event.target.parentNode.children;
     if (event.target.classList.contains('star')) {
       for (let a = 0; a < children.length; a++) {
-        children[a].classList.remove('hoverOn');
+        children[a].classList.remove('hover-on');
       }
     }
   }
